6fe171f7914daa711bdae7afd19872d6cdd316da
Petr Mitin <petr09mitin@mail.ru>
2020-11-13 17:23:49 +0300

    finished backend part

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const mongoose = require('mongoose');
 const app = express();
 
 // Basic Configuration
@@ -8,16 +9,71 @@ const port = process.env.PORT || 3000;
 
 app.use(cors());
 
+// body-parser is enbuilt in express
+app.use(express.urlencoded({extended: true}))
+
 app.use('/public', express.static(`${process.cwd()}/public`));
 
 app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-// Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
-});
+//Mongoose setup and functions
+const urlSchema = new mongoose.Schema({
+  'full_url': {type: String, required: true},
+  '_id': Number
+})
+
+const Url = mongoose.model('Url', urlSchema);
+
+mongoose.connect('mongodb+srv://petr:150600Pm@url-db.c0vis.mongodb.net/<dbname>?retryWrites=true&w=majority', {useNewUrlParser: true, useUnifiedTopology: true})
+
+const getPrevId = async () => {
+  //.exec() returns either nothing or Promise fullfilling to the requested data. 
+  //Do NOT use callback inside of the .exec() method. "await" it instead.
+  let data = await Url.find({}).sort([['_id', -1]]).exec();
+  if (!data[0]) {
+    return 0
+  }
+  return data[0]._id
+}
+
+// mounting route /api/shorturl/...
+app.post('/api/shorturl/new', async (req, res) => {
+  const url_to_shorten = req.body.url;
+  const prevId = await getPrevId();
+  Url.create({
+    'full_url': url_to_shorten,
+    '_id': prevId + 1
+  }, (err, data) => {
+    if (err) return console.log(err);
+    res.json({
+      full_url: url_to_shorten,
+      short_url: data._id
+    })
+  })
+})
+
+app.get('/api/shorturl/all', (req, res) => {
+  Url.find({}, (err, data) => {
+    if (err) return console.log(err);
+    res.json({"urls": data});
+  })
+})
+
+app.param('url', async (req, res, next, id) => {
+  const full_url = await Url.findOne({'_id': id}, (err, data) => {
+    if (err) return console.log(err);
+    return data.full_url;
+  });
+  req.body.full_url = full_url.full_url;
+  next();
+})
+
+app.get('/api/shorturl/:url', (req, res) => {
+  const url = req.body.full_url;
+  res.redirect(url)
+})
 
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
